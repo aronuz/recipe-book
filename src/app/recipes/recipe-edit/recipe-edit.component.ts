817f1271f6be4e5ac5ea8ebe788282902e50b335
Anatoliy <aronuz1@hotmail.com>
2019-08-07 13:09:37 -0400

    typo in recipe edit component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -45,7 +45,7 @@ export class RecipeEditComponent implements OnInit {
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'recipeDescription': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription)
     });
   }
 
