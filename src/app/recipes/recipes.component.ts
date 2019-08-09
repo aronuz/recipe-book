65fca28b9b219196e45b758d1bc09a6a461e019b
Anatoliy <aronuz1@hotmail.com>
2019-08-09 13:21:33 -0400

    img preview, recipe service declared in app module

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { RecipeService } from './recipe.service';
+//import { RecipeService } from './recipe.service'; recipe service component is destroyed when navigate to shopping component
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService]
+  //providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
   constructor() { }
