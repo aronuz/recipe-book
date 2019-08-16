6f5327aa76333d0b7b01083dacd182ab5a267a5d
Anatoliy <aronuz1@hotmail.com>
2019-08-15 22:34:02 -0400

    creating/storing user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,24 +1,35 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 @Injectable({providedIn: 'root'})
 export class AuthService{
+    user = new Subject<User>();
     constructor(private http: HttpClient){}
 
     signup(email: string, password: string){
        return this.http.post('https://', {email: email, password: password}).pipe(
-           catchError(this.handleError)
+            catchError(this.handleError), tap(responseData => {
+                this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+            })
        );
     }
 
     login(email: string, password: string){
         return this.http.post('https://', {email: email, password: password}).pipe(
-            catchError(this.handleError)
+            catchError(this.handleError), tap(responseData => {
+                this.handleAuthentication(responseData.email, responseData.localId, responseData.idToken, +responseData.expiresIn);
+            })
         );
     }
 
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000); //time from 1970 in ms + s * 1000
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
+    }
     private handleError(errorResponse: HttpErrorResponse){
         let errorMessage = "Unknown Error.";
         if(!errorResponse.error || !errorResponse.error.error){
